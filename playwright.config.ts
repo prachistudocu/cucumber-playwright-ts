a9333592127d76d67032adf00ca10921fcbb030f
Daniel Castro <100843615+danielcastro-studocu@users.noreply.github.com>
2022-08-18 17:52:23 +0200

    Removing unnecessary empty lines

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,12 @@
 import {devices, type PlaywrightTestConfig} from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-
     use: {
         navigationTimeout: 20000,
         trace: 'on-first-retry',
         ignoreHTTPSErrors: true,
         screenshot: 'only-on-failure',
     },
-
     projects: [
         {
             name: 'chromium',
@@ -18,15 +16,14 @@ const config: PlaywrightTestConfig = {
             name: 'webkit',
             use: {...devices['Desktop Safari']},
         },
-
         {
             name: 'mobile',
             use: {
                 browserName: 'webkit',
                 ...devices['iPhone 8']
             }
-
         },
     ],
 };
+
 export default config;
