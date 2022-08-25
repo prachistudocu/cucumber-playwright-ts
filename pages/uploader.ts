a0e55ac4bf67fa96cf6ac658918e2e6751d2c7d6
Daniel Castro <daniel.castro@studocu.com>
2022-08-25 12:21:46 +0200

    Implementing default values in the method signature.

--- pages/uploader.ts
+++ pages/uploader.ts
@@ -12,9 +12,8 @@ const openOrganicUploader = async (page: Page) => {
     await waitForVisible(page, 'documents-drop-area');
 }
 
-const uploadAndSubmitDocument = async (page: Page, name: string) => {
-    const fileName = name == null ? 'Atomic_number.pdf' : name;
-    await page.setInputFiles('input[type="file"]', fileName);
+const uploadAndSubmitDocument = async (page: Page, filename: string = 'Atomic_number.pdf') => {
+    await page.setInputFiles('input[type="file"]', filename);
     
     await waitForVisible(page, 'uploaded-document-name');
     await clickOnElement(page, 'submit-uploads-button');
@@ -23,7 +22,7 @@ const uploadAndSubmitDocument = async (page: Page, name: string) => {
     await waitForVisible(page, 'upload-step-2');
 }
 
-const fillInDocumentDetails = async (page: Page, universityName: string, courseName: string, categoryName: string, academicYear: string) => {
+const fillInDocumentDetails = async (page: Page, universityName: string = 'Universiteit Leiden', courseName: string = 'Organische Chemie', categoryName: string = 'Lecture notes', academicYear: string = '2020/2021') => {
     const longDescription = 'Description is the fiction-writing mode for transmitting a mental image of the particulars of a story.';
     const longTitle = ['Title "', '" based in the filename is too short, so it was renamed!'];
 
@@ -40,8 +39,7 @@ const fillInDocumentDetails = async (page: Page, universityName: string, courseN
     }
 
     // Select year
-    const academicYearLabel = academicYear == null ? '2020/2021' : academicYear;
-    await page.selectOption('[data-test-selector="academic-year-select"]', {label: academicYearLabel});
+    await page.selectOption('[data-test-selector="academic-year-select"]', {label: academicYear});
 
     // Select Summary content "Module"
     if (categoryName === 'Summaries') {
@@ -52,19 +50,17 @@ const fillInDocumentDetails = async (page: Page, universityName: string, courseN
     await fillInInput(page, 'upload-document-description', longDescription);
 }
 
-const selectUniversity = async (page: Page, name: string) => {
+const selectUniversity = async (page: Page, universityName: string) => {
     await waitForVisible(page, 'uploader-institution-input');
     await page.waitForTimeout(2500);
     await clickOnElement(page, 'uploader-institution-input', {delay: 1000});
-    const universityName = name == null ? 'Universiteit Leiden' : name;
     await fillInInput(page, 'uploader-institution-input', universityName);
     await clickOnElement(page, 'uploader-institution-input');
     await clickOnElement(page, 'uploader-institution-result-0');
     await waitForVisible(page, 'institution-picker-selected-institution');
 }
 
-const selectCourse = async (page: Page, name: string) => {
-    const courseName = name == null ? 'Organische Chemie' : name;
+const selectCourse = async (page: Page, courseName: string) => {
     await fillInInput(page, 'uploader-course-input', courseName);
     await clickOnElement(page, 'uploader-course-input');
     await waitForVisible(page, 'uploader-course-results');
@@ -72,8 +68,7 @@ const selectCourse = async (page: Page, name: string) => {
     await waitForVisible(page, 'course-picker-edit-button');
 }
 
-const selectCategory = async (page: Page, name: string) => {
-    const categoryName = name == null ? 'Lecture notes' : name;
+const selectCategory = async (page: Page, categoryName: string) => {
     await page.selectOption('[data-test-selector="category-select-dropdown"]', {label: categoryName});
 }
 
--- tests/uploader.spec.ts
+++ tests/uploader.spec.ts
@@ -7,8 +7,8 @@ const categories = ['Lecture notes', 'Other'];
 for (const category of categories) {
     test(`organic uploader with ${category}`, async ({page}) => {
         await uploader_page.openOrganicUploader(page);
-        await uploader_page.uploadAndSubmitDocument(page, null);
-        await uploader_page.fillInDocumentDetails(page, null, null, category, null);
+        await uploader_page.uploadAndSubmitDocument(page, undefined);
+        await uploader_page.fillInDocumentDetails(page, undefined, undefined, category, undefined);
         await clickOnElement(page, 'submit-documents', {delay: 1500});
 
         await page.waitForURL('**/upload/complete**');
